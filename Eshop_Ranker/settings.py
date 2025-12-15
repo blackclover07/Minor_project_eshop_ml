bcd1706f19f80d994e479dfdfaae1bb156e13f9f
Sagar Dey <deysagar988@gmail.com>
2025-12-16 00:48:57 +0530

    update the requiremenmts.txt add cors policy

--- Eshop_Ranker/settings.py
+++ Eshop_Ranker/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'home',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
